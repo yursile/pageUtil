4e5222951c632fd1070b24e56072699a911a8077
yursile <289181450@qq.com>
2016-01-05 18:15:20 +0800

    o

---
+++ pageUtil.js
@@ -0,0 +1,14 @@
+function pageUtil(selector){
+	this.ele = document.querySelectorAll(selector)[0];
+	this.scaleNum = document.documentElement.clientWidth/640;
+	return this;
+}
+pageUtil.prototype.response = function(selector){
+    this.ele.style.cssText = "-webkit-transform-origin: 0 0 0px; -webkit-transform: scale("+this.scaleNum+");position: absolute;width:640px;height:1136px";
+    return this;
+}
+pageUtil.prototype.rotate = function(){
+	this.ele["style"]["cssText"] = "width: "+1136+"px;height: "+640+"px;transform:rotate(90deg) translate(-50%,-50%);"+
+	"transform-origin:0 0; position: absolute; top: 50%; left: 50%;"
+	return this;
+}
